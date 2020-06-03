8016ebb704569907bc4356391576b28abeecad41
Salman102 <51132743+Salman102@users.noreply.github.com>
2020-06-04 00:35:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World Salman and Usman</h1>
+      <h1>Hello World Salman and All</h1>
     </div>
   );
 }
